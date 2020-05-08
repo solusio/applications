81b5706c03919658fc7654d51d00b4c292adc9c6
Dmitrii Shemin <dshemin@plesk.com>
2020-05-08 20:12:08 +0700

    Small fixes in Plesk and Wordpress

--- plesk/assets/login.js
+++ plesk/assets/login.js
@@ -1,32 +1,24 @@
-let username = 'admin';
-let password = '{{ passwd }}';
-
-
-fetch("https://{{ domain }}:8443/api/v2/cli/admin/call", {
-  body: "{ \"params\": [ \"--get-login-link\" ] }",
-   headers: {
-    Authorization: "Basic "+ btoa(username+":"+password),
+fetch('https://{{ domain }}:8443/api/v2/cli/admin/call', {
+  body: '{ "params": [ "--get-login-link" ] }',
+  headers: {
+    Authorization: 'Basic '+ btoa('admin:{{ passwd }}'),
     "Content-Type": "application/json"
   },
   method: "POST"
 })
-  .then(function(response){
-	if(!response.ok){
-    throw Error(response.statusText);
+  .then(function(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
     }
   })
-  .then((data) => {
-	var obj = data.stdout;
-	var link = obj.split(/\r?\n/);
-	openWindow(link[0]);
-	end();
-  }).catch(function(error){
-
-                
-           try{
-    openWindow("https://{{ domain }}:8443/login_up.php3?login_name=admin&passwd={{ passwd }}");
-		   }
-	catch(err){
-		openWindow("https://{{domain}}:8443/");
-	}
+  .then(function(data) {
+    var link = data.stdout.split(/\r?\n/);
+    openWindow(link[0]);
+  })
+  .catch(function() {
+    try {
+      openWindow('https://{{ domain }}:8443/login_up.php3?login_name=admin&passwd={{ passwd }}');
+    } catch (err) {
+      openWindow('https://{{domain}}:8443/');
+    }
   });
